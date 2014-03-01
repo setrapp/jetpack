7374e3be3c23043c3aa3a1bcdfd5bf4cc3ef305c
Sam Trapp <trapp.s.e@gmail.com>
2014-02-28 23:43:31 -0500

    Made shifting horizontally work better by only updating oldMousePos one axis at a time.

--- prototype/Assets/Scripts/Shifter.cs
+++ prototype/Assets/Scripts/Shifter.cs
@@ -5,10 +5,8 @@ public class Shifter : MonoBehaviour {
 	private bool shifting = false;
 	public GameObject hpattern = null;
 	public GameObject stick = null;
-	public Camera camera = null;
 	public MouseLook look = null;
 	private bool blocked = false;
-	//private Vector3 oldMouseWorldPos;
 	private Vector2 oldMousePos;
 	public float mouseMoveThreshold;
 	public ShifterSpots shifterSpots;
@@ -24,80 +22,32 @@ public class Shifter : MonoBehaviour {
 				oldMousePos = Input.mousePosition;
 				hpattern.SetActive(true);
 				shifterSpots.MoveShifter(ShifterSpots.ShifterDirection.NONE, stick);
-				/* Move shift under mouse, do we want this?
-				Vector3 hpatternToStick = stick.transform.position - hpattern.transform.position;
-				Vector3 cameraToStick = stick.transform.position - camera.transform.position;
-				stick.transform.position = camera.ScreenToWorldPoint(new Vector3 ((float)Input.mousePosition.x, (float)Input.mousePosition.y, camera.transform.TransformDirection(cameraToStick).z));
-				stick.transform.Translate(cameraToStick);
-				//oldMouseWorldPos = camera.ScreenToWorldPoint(new Vector3 ((float)Input.mousePosition.x, (float)Input.mousePosition.y, stick.transform.position.z - camera.transform.position.z));
-				//PutStickAtMouse (false);
-				hpattern.transform.position = stick.transform.position - hpatternToStick;
-				*/
 				shifting = true;
 			} else {
 				if (Input.mousePosition.y - oldMousePos.y < -mouseMoveThreshold) {
-					if (shifterSpots.MoveShifter(ShifterSpots.ShifterDirection.DOWN, stick)) {
-						oldMousePos = Input.mousePosition;
-					}
+					shifterSpots.MoveShifter(ShifterSpots.ShifterDirection.DOWN, stick);
+					oldMousePos.y = Input.mousePosition.y;
 				} else if (Input.mousePosition.y - oldMousePos.y > mouseMoveThreshold) {
-					if (shifterSpots.MoveShifter(ShifterSpots.ShifterDirection.UP, stick)) {
-						oldMousePos = Input.mousePosition;
-					}
+					shifterSpots.MoveShifter(ShifterSpots.ShifterDirection.UP, stick);
+					oldMousePos.y = Input.mousePosition.y;
 				}
 
 				if (Input.mousePosition.x - oldMousePos.x < -mouseMoveThreshold) {
-					if (shifterSpots.MoveShifter(ShifterSpots.ShifterDirection.LEFT, stick)) {
-						oldMousePos = Input.mousePosition;
-					}
+					shifterSpots.MoveShifter(ShifterSpots.ShifterDirection.LEFT, stick);
+					oldMousePos.x = Input.mousePosition.x;
 				} else if (Input.mousePosition.x - oldMousePos.x > mouseMoveThreshold) {
-					if (shifterSpots.MoveShifter(ShifterSpots.ShifterDirection.RIGHT, stick)) {
-						oldMousePos = Input.mousePosition;
-					}
+					shifterSpots.MoveShifter(ShifterSpots.ShifterDirection.RIGHT, stick);
+					oldMousePos.x = Input.mousePosition.x;
 				}
 
 
 			}
 		} else if (shifting){
 			hpattern.SetActive(false);
-			//PutStickAtMouse(false);
 			shifting = false;
 		}
 
 		look.enabled = !shifting;
-	}
-
-	private void PutStickAtMouse(/*bool keepInBox*/) {
-		//Vector3 stickPos = shifterSpots
-		/*Vector3 mouseWorldPos = camera.ScreenToWorldPoint(new Vector3 ((float)Input.mousePosition.x, (float)Input.mousePosition.y, stick.transform.position.z - camera.transform.position.z));
-		Vector3 stickPos = stick.transform.position + mouseWorldPos - oldMouseWorldPos;
-		//stickPos.z = stick.transform.position.z;
-
-		bool tempBlocked = blocked;
-		RaycastHit hit = new RaycastHit();
-		Vector3 raycastOrigin = stick.transform.position;
-		raycastOrigin.z = hpattern.transform.position.z;
-		Vector3 toStickPosDir = stickPos - stick.transform.position;
-		float toStickPosDist = toStickPosDir.magnitude;
-		if (toStickPosDist > 0) {
-			toStickPosDir /= toStickPosDist;
-			if (Physics.Raycast(raycastOrigin, toStickPosDir, out hit, toStickPosDist, ~LayerMask.NameToLayer("Shifterbox"))) {
-				//stickPos = hit.point - (toStickPosDir / 100);
-				//stick.transform.position = hit.point;
-				//tempBlocked = true;
-				//Debug.Log ("blocked");
-			} else {
-				tempBlocked = false;
-			}
-		}
-
-		if (!blocked) {
-			//stickPos.z = stick.transform.position.z;
-			stick.transform.position = stickPos;
-		} else {
-			//mouseOffset = 
-		}
-		blocked = tempBlocked;
-		oldMouseWorldPos = mouseWorldPos;*/
 	}
 }
 
--- prototype/Assets/Scripts/Tachometer.cs
+++ prototype/Assets/Scripts/Tachometer.cs
@@ -21,13 +21,12 @@ public class Tachometer : MonoBehaviour
 		} else if (jetpack.inGear == 6) {
 			gear = "Hover";
 		}
-		//GUI.Box(new Rect (150, 10, 200, 20));
 		GUI.DrawTexture (new Rect (15, 10, 20, 200), background);
 		float foregroundHeight = 200 * Mathf.Clamp (criticalRatio, 0.0f, 1.0f);
 		Texture renderedForeground = (jetpack.inGear != 0 && (criticalRatio < dangerRatio || criticalRatio > 1.0f - dangerRatio)) ? dangerForeground : foreground;
-		//Debug.Log ((criticalRatio < dangerRatio || criticalRatio > 1.0f - dangerRatio));
 		GUI.DrawTexture (new Rect (15, 10 + (200 - foregroundHeight), 20, foregroundHeight), renderedForeground);
 		GUI.Label (new Rect (10, 220, 200, 20), "RPM?");
+		GUI.Label (new Rect (10, 250, 200, 20), "Gear: " + gear);
 		/*GUI.Label (new Rect (150, 40, 200, 20), "Min Speed: " + (int)jetpack.gears[jetpack.inGear].minSpeed);
 		GUI.Label (new Rect (150, 70, 200, 20), "Cur Speed: " + (int)jetpack.jetpackSpeed);
 		GUI.Label (new Rect (150, 100, 200, 20), "Max Speed: " + (int)jetpack.gears[jetpack.inGear].maxSpeed);
