9867ec76094ec4180a471d79ff77be0dcb040e8a
unknown <ajh2510@GDD-27.main.ad.rit.edu>
2014-03-06 16:39:38 -0500

    Added nick's project to git

---
+++ NickJetpack/Assets/Scripts/DeathRockets.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathRockets : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(transform.position.y <= -8){
+			Destroy(gameObject);
+		}
+	}
+}
